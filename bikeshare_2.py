d5da7499e31afa62244f78c013fb383799c42b45
Lisa Boenisch <lisa.boenisch@kit.edu>
2021-04-24 15:50:27 +0200

    Remove empty lines

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -9,7 +9,6 @@ CITY_DATA = { 'Chicago': 'chicago.csv',
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
-
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
@@ -79,7 +78,6 @@ def get_filters():
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
-
     Args:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
@@ -220,12 +218,10 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
